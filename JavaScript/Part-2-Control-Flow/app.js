a58fca2d0835b25a4e383fd2e7f74b352e13e0f0
Ashish Rodale <ashishrodale18@gmail.com>
2026-08-10 18:04:24 +0530

    feat(js-part2): add main summary script

---
+++ JavaScript/Part-2-Control-Flow/app.js
@@ -0,0 +1,40 @@
+// Arithmatic Operators
+let a  = 10;
+let b = 5;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
+
+//Unary Oerator
+console.log(a++);
+console.log(++a);
+console.log(a--);
+console.log(--a);
+
+//Assignment Operator
+console.log(a=b);
+console.log(a+=b);
+console.log(a-=b);
+console.log(a*=b);
+console.log(a/=b);
+console.log(a%=b);
+
+//Comparison Operator
+let age = 18;
+console.log(age>18);
+console.log(age>=18);
+console.log(age<18);
+console.log(age<=18);
+console.log(age==18);
+console.log(age!=18);
+
+//Conditional statements
+console.log("before my if statement");
+let age1 = 18;
+if(age1>=18){
+    console.log("You can vote");
+}
+console.log("after my if statement");

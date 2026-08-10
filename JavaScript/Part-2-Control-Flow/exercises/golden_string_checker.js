71dc7b6a3607499789fba3853394ea1197bfd573
Ashish Rodale <ashishrodale18@gmail.com>
2026-08-10 18:07:04 +0530

    Add golden string checker program

---
+++ JavaScript/Part-2-Control-Flow/exercises/golden_string_checker.js
@@ -0,0 +1,10 @@
+// Question - A "good string" is a string that starts with the letter'a' and has a length >3.
+//Write a program to find if a string is good or not.
+
+let str = "apple";
+
+if((str[0] === 'a') && (str.length > 3)) {
+    console.log("good string");
+} else{
+    console.log("not a good string")
+}

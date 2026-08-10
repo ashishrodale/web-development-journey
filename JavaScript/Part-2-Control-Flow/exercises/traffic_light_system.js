2d43f1d22f0049c86e6a4db4ebd285f068c3f51a
Ashish Rodale <ashishrodale18@gmail.com>
2026-08-10 18:05:43 +0530

    feat(js-part2): add traffic light system practice exercise
    Implemented a traffic light system that outputs actions based on the current light color.

---
+++ JavaScript/Part-2-Control-Flow/exercises/traffic_light_system.js
@@ -0,0 +1,14 @@
+//Create a traffic light system that shows what to do based on colors...
+
+let color = "red";
+
+//Traffic Light System
+if(color==="red"){
+    console.log("Stop! light color is red");
+}else if(color==="yellow"){
+    console.log("Slow down light color is yellow");
+}else if(color==="green"){
+    console.log("Go light color is green");
+} else{
+    console.log("trffic light is broken");
+}

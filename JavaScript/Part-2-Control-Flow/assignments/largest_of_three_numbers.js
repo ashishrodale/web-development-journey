5fa310972012d102342a2771f9f237d35fc4ba8d
Ashish Rodale <ashishrodale18@gmail.com>
2026-08-11 06:05:49 +0530

    Create largest_of_three_numbers.js

---
+++ JavaScript/Part-2-Control-Flow/assignments/largest_of_three_numbers.js
@@ -0,0 +1,18 @@
+// Question - Write a program to find the largest of 3 numbers.
+
+let a = 5;
+let b = 18;
+let c = 13;
+if(a>b){
+    if(a>c){
+        console.log(a,"is largest");
+    } else {
+        console.log(c,"is largest");
+    }
+} else { 
+    if(b>c){
+        console.log(b,"is largest");
+    } else {
+        console.log(c,"islargest");
+    }
+}    

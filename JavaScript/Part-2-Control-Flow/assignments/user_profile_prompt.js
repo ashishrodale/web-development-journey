639394a176bdf85daeda9d5194f6aa1f4dda295a
Ashish Rodale <ashishrodale18@gmail.com>
2026-08-11 06:03:26 +0530

    Add user profile prompt for name and age

---
+++ JavaScript/Part-2-Control-Flow/assignments/user_profile_prompt.js
@@ -0,0 +1,7 @@
+// Question - Take the user 's name & age as input using prompts.
+// Then return back the following statement to the user as an alert(by substituting their name & age):
+
+let name = prompt("Enter your name");
+let $age = prompt("Enter your age");
+
+alert('4(name) is $($age) years old');

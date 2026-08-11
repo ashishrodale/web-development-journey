c455a5faddaee0a4a6d9cb9b9918ca179594bc95
Ashish Rodale <ashishrodale18@gmail.com>
2026-08-11 06:04:04 +0530

    Implement switch statement for quarter months
    Fixed syntax errors in switch cases and added proper spacing.

---
+++ JavaScript/Part-2-Control-Flow/assignments/quarter_months_switch.js
@@ -0,0 +1,21 @@
+//Question - Write a switch statement to print the months in a quarter .
+// Months in Quarter 1 :January,February,March Months in Quarter2:April,May,June Months in Quarter 3:July,August,September Months in 
+// Quarter 4:October,November,December [Use the number as the case value in switch].
+
+let quarter = 1;
+switch(quarter){
+    case 1: 
+    console.log("January,February,March");
+    break;
+    case2:
+    console.log("April,May,June");
+    break;
+    case3:
+    console.log("July,August,September");
+    break;
+    case4:
+    console.log("October,November,December");
+    break;
+    default:
+        console.log("NOTAQUARTER!");
+}

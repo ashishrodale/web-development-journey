177c7cb4e54c33b6e5cad9c513438fba4664ac37
Ashish Rodale <ashishrodale18@gmail.com>
2026-08-13 06:22:30 +0530

    Create checkElementExists.js

---
+++ JavaScript/Part-3-Arrays-And-Strings/assignments/checkElementExists.js
@@ -0,0 +1,5 @@
+// Q6: Check if an element exists in an array
+function checkElementExists(arr, element) {
+    return arr.includes(element);
+}
+console.log("Q6 Output:", checkElementExists(["HTML", "CSS", "JS"], "JS")); // true

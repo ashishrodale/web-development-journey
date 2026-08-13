dda1a4665a22aa04707c152dbf450cfd35be8b19
Ashish Rodale <ashishrodale18@gmail.com>
2026-08-13 06:20:35 +0530

    Create getFirstNElements.js

---
+++ JavaScript/Part-3-Arrays-And-Strings/assignments/getFirstNElements.js
@@ -0,0 +1,5 @@
+// Q1: Get first n elements of an array
+function getFirstNElements(arr, n) {
+    return arr.slice(0, n);
+}
+console.log("Q1 Output:", getFirstNElements([7, 9, 0, -2], 3)); // [7, 9, 0]

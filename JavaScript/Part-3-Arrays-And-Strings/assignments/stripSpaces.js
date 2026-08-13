9f28273a4fabe5e6a9eaf94f1b70e6cc37b1884b
Ashish Rodale <ashishrodale18@gmail.com>
2026-08-13 06:22:07 +0530

    Create stripSpaces.js

---
+++ JavaScript/Part-3-Arrays-And-Strings/assignments/stripSpaces.js
@@ -0,0 +1,5 @@
+// Q5: Strip leading and trailing spaces from a string
+function stripString(str) {
+    return str.trim();
+}
+console.log("Q5 Output:", `"${stripString("   Hello World   ")}"`); // "Hello World"

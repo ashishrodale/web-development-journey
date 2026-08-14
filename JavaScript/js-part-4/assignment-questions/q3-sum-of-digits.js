a6e7df8c806e313acc5b1c8ecf647b4f107cc0ec
Ashish Rodale <ashishrodale18@gmail.com>
2026-08-14 06:20:55 +0530

    Implement sum of digits calculation
    Fix variable name 'digits' to 'digit' in sum calculation.

---
+++ JavaScript/js-part-4/assignment-questions/q3-sum-of-digits.js
@@ -0,0 +1,15 @@
+// Question - Write a JS program to find the sum of digits in a number.
+// Example : if number = 287152, sum=25
+
+let number = 287152;
+let sum = 0;
+
+let copy = number;
+
+while(copy > 0){
+    digit = copy % 10;
+    sum += digits;
+    copy = Math.floor(copy/10);
+}
+
+console.log(sum);

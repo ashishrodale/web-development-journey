90d86fc989b8e43715b3bed1a117e5fe808ca839
Ashish Rodale <ashishrodale18@gmail.com>
2026-08-15 08:45:56 +0530

    Create 04-multiplication-table.js

---
+++ JavaScript/js-part-4/practice-concepts/04-multiplication-table.js
@@ -0,0 +1,6 @@
+// Topic: Printing Multiplication Tables Dynamically
+console.log("--- Multiplication Table for 5 ---");
+const number = 5;
+for (let i = 1; i <= 10; i++) {
+    console.log(`${number} x ${i} = ${number * i}`);
+}

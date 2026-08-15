b860ed602c1e76166949c4ceaa354bea1c6235ec
Ashish Rodale <ashishrodale18@gmail.com>
2026-08-15 08:44:20 +0530

    Create 03-print-odd-even-numbers.js

---
+++ JavaScript/js-part-4/practice-concepts/03-print-odd-even-numbers.js
@@ -0,0 +1,10 @@
+// Topic: Filtering Odd & Even Numbers using Loops
+console.log("--- Odd Numbers (1 to 15) ---");
+for (let i = 1; i <= 15; i += 2) {
+    console.log(i);
+}
+
+console.log("--- Even Numbers (2 to 10) ---");
+for (let i = 2; i <= 10; i += 2) {
+    console.log(i);
+}

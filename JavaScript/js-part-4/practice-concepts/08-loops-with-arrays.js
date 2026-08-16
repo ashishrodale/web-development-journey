b6c079c037748c400cfa9f1e90f42377dd7109bb
Ashish Rodale <ashishrodale18@gmail.com>
2026-08-16 09:10:13 +0530

    Create 08-loops-with-arrays.js

---
+++ JavaScript/js-part-4/practice-concepts/08-loops-with-arrays.js
@@ -0,0 +1,7 @@
+// Topic: Iterating Arrays with Loops
+console.log("--- Array Iteration ---");
+const fruits = ["mango", "apple", "banana", "litchi", "orange"];
+
+for (let i = 0; i < fruits.length; i++) {
+    console.log(`Index ${i}: ${fruits[i]}`);
+}

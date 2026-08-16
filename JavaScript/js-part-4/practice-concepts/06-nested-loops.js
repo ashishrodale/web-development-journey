f04ae879fed4770bca8635060a88d9f0b4aadc70
Ashish Rodale <ashishrodale18@gmail.com>
2026-08-16 09:09:25 +0530

    Create 06-nested-loops.js

---
+++ JavaScript/js-part-4/practice-concepts/06-nested-loops.js
@@ -0,0 +1,8 @@
+// Topic: Nested For Loops
+console.log("--- Nested Loops Demo ---");
+for (let i = 1; i <= 3; i++) {
+    console.log(`Outer Loop: ${i}`);
+    for (let j = 1; j <= 2; j++) {
+        console.log(`   Inner Loop: ${j}`);
+    }
+}

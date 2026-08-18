826278ca761ede583102e905b2af76ae8d45597e
Ashish Rodale <ashishrodale18@gmail.com>
2026-08-18 19:01:08 +0530

    Create q3-person-object.js

---
+++ JavaScript/part-5/assignments/q3-person-object.js
@@ -0,0 +1,12 @@
+// Qs3: Create an object Person with name, age, city.
+// Edit city to "New York" and add country "United States".
+const Person = {
+    name: "JaneDoe",
+    age: 21,
+    city: "New Jersey"
+};
+
+Person.city = "New York";
+Person.country = "United States";
+
+console.log(Person);

df40ef15394195a8a8dcd80d0753e0d70e51896a
Ashish Rodale <ashishrodale18@gmail.com>
2026-08-18 18:59:44 +0530

    Create q1-dice-roll.js

---
+++ JavaScript/part-5/assignments/q1-dice-roll.js
@@ -0,0 +1,3 @@
+// Qs1: Create a program that generates a random number representing a dice roll (1 to 6).
+let diceRoll = Math.floor(Math.random() * 6) + 1;
+console.log(`Dice Roll Outcome: ${diceRoll}`);

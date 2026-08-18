93d71678317469c35a51943aa1de2a182dc8564d
Ashish Rodale <ashishrodale18@gmail.com>
2026-08-18 19:00:21 +0530

    Create q2-car-object.js

---
+++ JavaScript/part-5/assignments/q2-car-object.js
@@ -0,0 +1,8 @@
+// Qs2: Create an object representing a car storing name, model, color.
+const car = {
+    name: "Maruti Suzuki",
+    model: "Maruti Suzuki Dzire",
+    color: "white"
+};
+
+console.log(car.name);

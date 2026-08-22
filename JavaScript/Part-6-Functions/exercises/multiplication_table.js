1cf09d3d6e305750719442f20437d7264c94e26c
Ashish Rodale <ashishrodale18@gmail.com>
2026-08-22 16:04:19 +0530

    Add multiplication table function
    Function prints the multiplication table for a given number.

---
+++ JavaScript/Part-6-Functions/exercises/multiplication_table.js
@@ -0,0 +1,9 @@
+// Question : Create a Function that prints the multiplication table  of a numbers.
+function printTable(n){
+    for(let i = 0; i<=n*10; i+=n){
+        console.log(i);
+        
+    }
+}
+
+printTable(5);

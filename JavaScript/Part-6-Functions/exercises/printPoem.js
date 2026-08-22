a25505d650a544358c5dd13b8f10311fe0d22d12
Ashish Rodale <ashishrodale18@gmail.com>
2026-08-22 16:02:26 +0530

    Create printPoem.js

---
+++ JavaScript/Part-6-Functions/exercises/printPoem.js
@@ -0,0 +1,11 @@
+// Question : Create a function that prints a poem.
+
+function printPoe() {
+    console.log("Twinkle Twinkle, little star'");
+    console.log("how I wonder what you are,");
+    console.log("Up above the world so high,");
+    console.log("Like a diamond in the sky.");  
+    
+}
+
+printPoe();

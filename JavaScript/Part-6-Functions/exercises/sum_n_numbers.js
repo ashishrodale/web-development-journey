a1b50eed77dde479aa0034fb5c67820120d6ca88
Ashish Rodale <ashishrodale18@gmail.com>
2026-08-22 16:04:46 +0530

    Create sum_n_numbers.js

---
+++ JavaScript/Part-6-Functions/exercises/sum_n_numbers.js
@@ -0,0 +1,10 @@
+// Question: Create a Function that returns the sum of number from 1 to n.
+
+function getSum(n){
+    let sum = 0;
+
+    for(let i=1; i<=n; i++){
+        sum += i;
+    }
+    return sum;
+}

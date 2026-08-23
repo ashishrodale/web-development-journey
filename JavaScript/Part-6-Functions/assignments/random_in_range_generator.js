bf413ed7b417eaf146ac7b7c0ba9870b3ebf6e10
Ashish Rodale <ashishrodale18@gmail.com>
2026-08-23 14:47:38 +0530

    Create random_in_range_generator.js

---
+++ JavaScript/Part-6-Functions/assignments/random_in_range_generator.js
@@ -0,0 +1,7 @@
+// Question - Write a JavaScript function to generate a random number with in a range(start,end).
+let start = 100;
+let end = 200;
+function generateRandom(start,end){
+    let diff = end-start;
+    return Math.floor(Math.random()*diff)+start;
+}

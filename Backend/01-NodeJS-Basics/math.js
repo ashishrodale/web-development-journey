fcc316357e1f117e3845620373a5a7857a7c7664
Ashish Rodale <ashishrodale18@gmail.com>
2026-09-04 06:28:56 +0530

    Add math module with add and multiply functions

---
+++ Backend/01-NodeJS-Basics/math.js
@@ -0,0 +1,12 @@
+// Module Exports & Function Definitions
+
+const add = (a, b) => a + b;
+const multiply = (a, b) => a * b;
+const PI = 3.14159;
+
+// Exporting functions and constants as a module object
+module.exports = {
+    add,
+    multiply,
+    PI
+};
